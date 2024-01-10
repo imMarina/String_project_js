3ad9ae4d1f297c43efce23492617fc65c7d852a7
imMarina <m.shebukhova@yandex.com>
2024-01-10 09:21:27 +0100

    Задание 12.9 draft

--- src/index.js
+++ src/index.js
@@ -363,3 +363,84 @@ btn8.addEventListener('click', () => {
   // очищаем поле ввода
   input8.value = "";
 });
+
+
+// Задание 12.9
+// На вход подаётся строка вида "12+23-4*5/10+11". Выведете на экран результат выражения. Приоритет знаков не имеет значения.
+
+// input
+// 12-10*2
+// output
+// 4
+
+// DOM elements
+const task9 = document.querySelector('.task-9');
+const input9 = task9.querySelector("#input"); // поиск по id
+const btn9 = task9.querySelector(".btn");
+const result9 = task9.querySelector('.result');
+
+
+
+let string = '12-10*2';
+
+  let stringToArray = string.split('');  
+// console.log(valueArr); // ['1', '2', '-', '1', '0', '*', '2']
+
+  const mathOperation = ['+', '-', '/', '*'];
+
+  const operationsIncludedArr = [];
+  const numArr = [];
+  
+  for (let item of stringToArray) {
+      if (!(parseInt(item) >= 0 && parseInt(item) < 10)) {
+          operationsIncluded.push(item);
+      }
+      
+      if (parseInt(item) >= 0 && parseInt(item) < 10) {
+          numArr.push(item);
+      } else {
+          numArr.push(' ');
+      }
+  }
+
+// console.log(operationsIncluded); // [ '-', '*' ]
+// console.log(numArr); // ['1', '2', ' ', '1', '0', ' ', '2'];
+
+
+let onlyNumArr = numArr.reduce(function (strNum1, strNum2) {
+  return strNum1 + strNum2;
+}).split(' ');  
+
+console.log(onlyNumArr); // [ '12', '10', '2' ]
+console.log(operationsIncludedArr); // [ '-', '*' ]
+
+let total;
+    
+for (let i = 0; i < onlyNumArr.length; i++) {
+
+    let result;
+    let num1 = Number(onlyNumArr[i]);
+
+    for (let i = 0; i < onlyNumArr.length; i++) {
+        
+        let num2 = Number(onlyNumArr[i+1]);
+        
+        if (operationsIncludedArr[i] === '+') {
+            result = num1 + num2;
+        }
+        
+        if (operationsIncludedArr[i] === '-') {
+            result = num1 - num2;
+        }
+        if (operationsIncludedArr[i] === '/') {
+            result = num1 / num2;
+        }
+        if (operationsIncludedArr[i] === '*') {
+            result = num1 * num2;
+        }
+    }
+    
+    total = result;
+        console.log(total);
+}
+   // console.log(total);
