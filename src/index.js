81f3d76323fbd0e6e0f74e9032a7b9bac1903c04
imMarina <m.shebukhova@yandex.com>
2024-01-13 10:21:54 +0100

    JS common code refactoring

--- src/index.js
+++ src/index.js
@@ -1,502 +1,111 @@
 import './index.scss';
 
+import { taskExample } from './js/task-example.js';
+import { taskOne } from './js/task-1.js';
+import { taskTwo } from './js/task-2.js';
+import { taskThree } from './js/task-3.js';
+import { taskFour } from './js/task-4.js';
+import { taskFive } from './js/task-5.js';
+import { taskSix } from './js/task-6.js';
+import { taskSeven } from './js/task-7.js';
+import { taskEight } from './js/task-8.js';
+
+
 const tasks = [
   {
     id: 'example',
     title: 'Example',
     subtitle: 'Пользователь вводит предложение. Сделайте reverse каждого слова и выведете на экран результат.',
+    function: taskExample,
   },
   {
     id: 1,
     title: 'Задача 12.1',
     subtitle: 'Спросите у пользователя телефонный номер и выведете его в формате: +79235050505',
+    function: taskOne,
   },
   {
     id: 2,
     title: 'Задача 12.2',
     subtitle: 'Пользователь вводит строку. Нужно сформировать и вывести массив из всех чисел в этой строке.',
+    function: taskTwo,
   },
   {
     id: 3,
     title: 'Задача 12.3',
     subtitle: 'Подсчитайте количество строчных и прописных букв в строке. <br/>Считается, что в строке могут быть только английские буквы и пробелы.',
+    function: taskThree,
   },
   {
     id: 4,
     title: 'Задача 12.4',
     subtitle: 'Выведете самое длинное слово в предложении (слова разбиты пробелами).',
+    function: taskFour,
   },
   {
     id: 5,
     title: 'Задача 12.5',
     subtitle: 'Пользователь вводит слово. Если оно является перевёртышем выведете true иначе false. <br/>Слово-перевёртыш - это слово, которое одинаково читается с начала и с конца. <br/>Слова могут быть как на английском, так и на русском.',
+    function: taskFive,
   },
   {
     id: 6,
     title: 'Задача 12.6',
     subtitle: 'Удалите лишние пробелы из строки. <br/>Пробелы лишние если они в начале или в конце строки, а также если между словами больше одного пробела. <br/>Считается, что строка состоит только из букв и пробелов.',
+    function: taskSix,
   },
   {
     id: 7,
     title: 'Задача 12.7',
     subtitle: 'Дана строка, сделайте первую букву каждого слова прописной (большой).',
+    function: taskSeven,
   },
   {
     id: 8,
     title: 'Задача 12.8',
     subtitle: 'На вход подаётся строка вида "12+23=35". <br/>Выведете на экран true если выражение верно или false если выражение не верно.',
+    function: taskEight,
   },
   {
     id: 9,
     title: 'Задача 12.9',
     subtitle: 'На вход подаётся строка вида "12+23-4*5/10+11". Выведете на экран результат выражения. <br/>Приоритет знаков не имеет значения.',
+    function: () => {},
   }
 ]
 
-tasks.forEach((task) => {
+tasks.forEach( (task) => {
 
   const taskTemplate = `
-  <div class="task-${task.id} task">
-    <div class="task-text">
-      <h2>${task.title}</h2>
-      <p>${task.subtitle}</p>
-    </div>
+    <div class="task-${task.id} task">
+      <div class="task-text">
+        <h2>${task.title}</h2>
+        <p>${task.subtitle || ''}</p>
+      </div>
 
-    <div class="input-form">
-      <label class="input-text" for="input"></label>
-      <input type="text" id="input" placeholder="${task.title}"> 
-      <button class="btn" type="button">Отправить</button>
-    </div>
+      <div class="input-form">
+        <label class="input-text" for="input-${task.id}"></label>
+        <input type="text" id="input-${task.id}" placeholder="${task.title}"> 
+        <button class="btn" type="button">Отправить</button>
+      </div>
 
-    <p class="result"></p>
-  </div>
+      <p class="result"></p>
+    </div>
   `;
 
-});
+  document.body.insertAdjacentHTML('beforeend', taskTemplate);
 
+  const taskElem = document.querySelector(`.task-${task.id}`);
+  const input = taskElem.querySelector(`#input-${task.id}`); // поиск по id
+  const btn = taskElem.querySelector(".btn");
+  const result = taskElem.querySelector('.result');
 
-btn.addEventListener("click", () => {
-	// из элемента <input> по ссылке input через свойство value
-	// получаем введённые данные из поля ввода 
-    const text = input.value;
+  btn.addEventListener("click", () => {
+      const string = input.value;
 
-	// проверяем, ввёл ли какой-либо текст пользователь
-    if (text) {
-		// разделяем предложение на слова и записываем их в массив words
-    const words = text.split(" ");
-		// создаём вспомогательный массив, в который будет хранить в себе перевёрнутые слова
-    const revWords = [];
-
-		// циклом идём по кадому слову в массиве words
-    for (let word of words) {
-			// вспомогательная переменная, в которую запишем новое перевёрнутое слово
-    let revWord = "";
-			// циклом идём по символам с конца слова word
-    for (let i = word.length - 1; i >= 0; --i) {
-				// записываем каждый символ с конца слова word в начало строки revWord 
-        revWord += word[i];
-    }
-			// добавляем новое первёрнутое слово revWord в массив revWords
-    revWords.push(revWord);
-    }
-		// соединяем все слова массива revWords в одну строку, соединительным символом является пробел " "
-    const revText = revWords.join(" ");
-		// добавляем в элемент <p> по ссылке result c помощью свойства textContent текст revText
-    result.textContent = revText;
-		// очищаем поле ввода
-    input.value = "";
-}
-});
-
-
-// Задача 12.1
-// Спросите у пользователя телефонный номер и выведете его в формате: `+79235050505`
-
-// DOM elements
-const task1 = document.querySelector('.task-1');
-const input1 = task1.querySelector("#input"); // поиск по id
-const btn1 = task1.querySelector(".btn");
-const result1 = task1.querySelector('.result');
-
-btn1.addEventListener("click", () => {
-
-  const string = input1.value;
-
-  let correctForm = '';
-  
-  for (let symbol of string) {
-    if (symbol === '8' && correctForm.length === 0) {
-      correctForm += '7';
-    } else if (parseInt(symbol) || symbol === '0') {
-      correctForm += symbol;
-    }
-  }
-
-  if (correctForm.length === 11) {
-    // Array to String conversion
-    result1.textContent = `+${correctForm}`;
-    }
-  
-  // очищаем поле ввода
-  input1.value = "";
-});
-
-
-
-// Задача 12.2
-// Пользователь вводит строку. Нужно сформировать и вывести массив из всех чисел в этой строке.
-
-// input 
-// 12 Один567два и три78
-// output
-// [ 12, 567, 78 ]
-
-// DOM elements
-const task2 = document.querySelector('.task-2');
-const input2 = task2.querySelector("#input"); // поиск по id
-const btn2 = task2.querySelector(".btn");
-const result2 = task2.querySelector('.result');
-
-btn2.addEventListener('click', () => {
-
-  const value = input2.value; // '12 0 Один567два и три78';
-
-  // String to Array conversion
-  const valueArr = value.split(' ');
-
-  // creating extra Array for 'numbers' only
-  let newArr = [];
-
-  // checking every Atring inside Array and removing not number chars
-  valueArr.forEach( (string) => {
-    let out = '';
-    for (let symbol of string) {
-
-        if (parseInt(symbol) || symbol === '0') {
-          out += symbol;
-      } 
-    }
-    newArr.push(parseInt(out));
-  });
-
-console.log(newArr); // [ 12, 0, 567, NaN, 78 ]
-
-// deleting empty strings
-const correctForm = newArr.filter((str) => str || str === 0);
-console.log(correctForm); // [ 12, 0, 567, 78 ]
-
-result2.textContent = correctForm;
-// очищаем поле ввода
-input2.value = "";
-});
-
-
-
-// Задание 12.3
-// Подсчитайте количество строчных и прописных букв в строке. Считается, что в строке могут быть только английские буквы и пробелы.
-
-// DOM elements
-const task3 = document.querySelector('.task-3');
-const input3 = task3.querySelector("#input"); // поиск по id
-const btn3 = task3.querySelector(".btn");
-const result3 = task3.querySelector('.result');
-
-btn3.addEventListener('click', () => {
-
-  let string = input3.value;
-
-  let countUpper = 0;
-  let countLower = 0;
-
-  for (let symbol of string) {
-          
-      if (symbol === symbol.toUpperCase() && symbol !== ' ') {
-          countUpper += 1;
-      } else if (symbol === symbol.toLowerCase() && symbol !== ' ') {
-          countLower += 1;
+      if (string) {
+        const correctForm = task.function(string);
+        result.textContent = correctForm;
+        input.value = "";
       }
-  }
-
-  let counter = `Прописных ${countUpper}\nСтрочных ${countLower}`;
-
-  result3.textContent = counter;
-  // очищаем поле ввода
-  input3.value = "";
-});
-
-
-
-// Задание 12.4
-// Выведете самое длинное слово в предложении (слова разбиты пробелами).
-
-// input
-// Один два три
-// output
-// Один
-
-// DOM elements
-const task4 = document.querySelector('.task-4');
-const input4 = task4.querySelector("#input"); // поиск по id
-const btn4 = task4.querySelector(".btn");
-const result4 = task4.querySelector('.result');
-
-btn4.addEventListener('click', () => { 
-
-  let value = input4.value;
-
-  // String to Array conversion
-  let valueArr = value.split(' ');
-  console.log(valueArr); // [ 'Один', 'два', 'три' ]
-  
-  // find max length
-  let max = 0;
-  let longestWord = '';
-  
-  for (let i = 0; i < valueArr.length; i++) {
-  
-        if (valueArr[i].length > max) {
-          max = valueArr[i].length;
-          longestWord += valueArr[i];
-        } 
-  
-    }
-
-  result4.textContent = `Слово: "${longestWord}", длина: ${longestWord.length}`;
-  // очищаем поле ввода
-  input4.value = "";
-});
-
-
-
-// Задание 12.5
-// Пользователь вводит слово. Если оно является перевёртышем выведете true иначе false. Слово-перевёртыш - это слово, которое одинаково читается с начала и с конца. Слова могут быть как на английском, так и на русском.
-
-// input
-// мадам
-//output
-// true
-
-// DOM elements
-const task5 = document.querySelector('.task-5');
-const input5 = task5.querySelector("#input"); // поиск по id
-const btn5 = task5.querySelector(".btn");
-const result5 = task5.querySelector('.result');
-
-btn5.addEventListener('click', () => { 
-
-  let value = input5.value;
-
-  // String to Array conversion
-  let valueArr = value.split('');
-  // console.log(valueArr); // [ 'м', 'а', 'д', 'а', 'м' ]
-
-  // reverse and Array to String conversion
-  let reverseWord = valueArr.toReversed().join('');
-
-  result5.textContent = value.toLowerCase() === reverseWord.toLowerCase() ? true : false;
-
-  // очищаем поле ввода
-  input5.value = "";
-});
-
-
-
-// Задание 12.6
-// Удалите лишние пробелы из строки. Пробелы лишние если они в начале или в конце строки, а также если между словами больше одного пробела. Считается, что строка состоит только из букв и пробелов.
-
-// input
-// Один     два  и три
-// output
-// Один два и три
-
-
-// DOM elements
-const task6 = document.querySelector('.task-6');
-const input6 = task6.querySelector("#input"); // поиск по id
-const btn6 = task6.querySelector(".btn");
-const result6 = task6.querySelector('.result');
-
-btn6.addEventListener('click', () => { 
-
-  let value = input6.value;
-
-  let result = value.split(' ')
-                    .filter((string) => string)
-                    .join(' ');
-
-  result6.textContent = result;
-  // очищаем поле ввода
-  input6.value = "";
-});
-
-
-// Задание 12.7
-// Дана строка, сделайте первую букву каждого слова прописной (большой).
-
-// DOM elements
-const task7 = document.querySelector('.task-7');
-const input7 = task7.querySelector("#input"); // поиск по id
-const btn7 = task7.querySelector(".btn");
-const result7 = task7.querySelector('.result');
-
-btn7.addEventListener('click', () => { 
-
-  let value = input7.value;
-
-  // String to Array conversion / + map method
-  let valueArr = value.split(' ').map( (string) => {
-        
-    let firstLetter = string[0].toUpperCase();
-    return string.replace(string[0], firstLetter);
-  });
-
-  // String to Array conversion
-  let result = valueArr.join(' ');
-
-  result7.textContent = result;
-  // очищаем поле ввода
-  input7.value = "";
-});
-
-
-
-// Задание 12.8
-// На вход подаётся строка вида "12+23=35". Выведете на экран `true` если выражение верно или `false` если выражение не верно.
-
-// DOM elements
-const task8 = document.querySelector('.task-8');
-const input8 = task8.querySelector("#input"); // поиск по id
-const btn8 = task8.querySelector(".btn");
-const result8 = task8.querySelector('.result');
-
-
-btn8.addEventListener('click', () => { 
-
-  let value = input8.value; // let value = '12+23=35';
-
-  let valueArr = value.split('=');  // [ '12+23', '35' ]
-
-  // console.log(valueArr[0]); // 12+23
-  // console.log(valueArr[valueArr.length - 1]); // 35
-
-  const mathOperation = ['+', '-', '/', '*', '**'];
-
-  const operation = mathOperation.find((op) => valueArr[0].includes(op)); // +
-
-  let indexOfMathSymbol = valueArr[0].indexOf(operation); // 2
-  
-  let num1 = parseInt(value.slice(0, indexOfMathSymbol)); // 12
-  let num2 = parseInt(value.slice(indexOfMathSymbol + 1)); // 23
-  let total = parseInt(valueArr[valueArr.length - 1]); // 35
-
-  let result;
-
-  switch (operation) {
-  case '+':
-    result = num1 + num2;
-    break;
-  case '-':
-    result = num1 - num2;
-    break;
-  case '/':
-    result = num1 / num2;
-    break;
-  case '*':
-    result = num1 * num2;
-    break;
-  case '**': // doesn't work this way ??? HELP
-    result = num1 ** num2;
-    break; 
-  }
-
-
-  if (result === total) {
-    result8.textContent = true;
-} else {
-    result8.textContent = false;
-}
-
-  // очищаем поле ввода
-  input8.value = "";
-});
-
-
-// Задание 12.9
-// На вход подаётся строка вида "12+23-4*5/10+11". Выведете на экран результат выражения. Приоритет знаков не имеет значения.
-
-// input
-// 12-10*2
-// output
-// 4
-
-// DOM elements
-const task9 = document.querySelector('.task-9');
-const input9 = task9.querySelector("#input"); // поиск по id
-const btn9 = task9.querySelector(".btn");
-const result9 = task9.querySelector('.result');
-
-
-
-let string = '12-10*2';
-
-  let stringToArray = string.split('');  
-// console.log(valueArr); // ['1', '2', '-', '1', '0', '*', '2']
-
-  const mathOperation = ['+', '-', '/', '*'];
-
-  const operationsIncludedArr = [];
-  const numArr = [];
-  
-  for (let item of stringToArray) {
-      if (!(parseInt(item) >= 0 && parseInt(item) < 10)) {
-          operationsIncluded.push(item);
-      }
-      
-      if (parseInt(item) >= 0 && parseInt(item) < 10) {
-          numArr.push(item);
-      } else {
-          numArr.push(' ');
-      }
-  }
-
-// console.log(operationsIncluded); // [ '-', '*' ]
-// console.log(numArr); // ['1', '2', ' ', '1', '0', ' ', '2'];
-
-
-let onlyNumArr = numArr.reduce(function (strNum1, strNum2) {
-  return strNum1 + strNum2;
-}).split(' ');  
-
-console.log(onlyNumArr); // [ '12', '10', '2' ]
-console.log(operationsIncludedArr); // [ '-', '*' ]
-
-let total;
-    
-for (let i = 0; i < onlyNumArr.length; i++) {
-
-    let result;
-    let num1 = Number(onlyNumArr[i]);
-
-    for (let i = 0; i < onlyNumArr.length; i++) {
-        
-        let num2 = Number(onlyNumArr[i+1]);
-        
-        if (operationsIncludedArr[i] === '+') {
-            result = num1 + num2;
-        }
-        
-        if (operationsIncludedArr[i] === '-') {
-            result = num1 - num2;
-        }
-        if (operationsIncludedArr[i] === '/') {
-            result = num1 / num2;
-        }
-        if (operationsIncludedArr[i] === '*') {
-            result = num1 * num2;
-        }
-    }
-    
-    total = result;
-        console.log(total);
-}
-   // console.log(total);
+  });});
