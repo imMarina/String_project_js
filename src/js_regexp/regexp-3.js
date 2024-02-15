48abe07005d457ccb59a6e7df78d42434413677d
imMarina <m.shebukhova@yandex.com>
2024-02-15 12:55:20 +0100

    regexExp_task_3

--- src/js_regexp/regexp-3.js
+++ src/js_regexp/regexp-3.js
@@ -1,28 +1,19 @@
 // Задание 12.3
 // Подсчитайте количество строчных и прописных букв в строке. Считается, что в строке могут быть только английские буквы и пробелы.
 
-export function regexpThree(string) {
+// input
+// One Two Three
+// output
+// Прописных: 3, строчных: 8
 
-    let countUpper = 0;
-    let countLower = 0;
 
-    const regexpUpper = /[A-Z]{1,}/g;
-    string.match(regexpUpper).forEach( (str) => {
-        countUpper += str.length;
-    });
-    // console.log(countUpper); // 3
-    
-    
-    const regexpLower = /[a-z]{1,}/g;
-    string.match(regexpLower).forEach( (str) => {
-        countLower += str.length;
-    });
-    // console.log(countLower); // 8
+export function regexpThree(string) { // 'One Two Three';
 
-    const counter = `Прописных ${countUpper}\nСтрочных ${countLower}`;
-    // console.log(counter); 
+    const up = string.match(/[A-Z]{1,}/g).join('').length; // 3
+    const low = string.match(/[a-z]{1,}/g).join('').length; // 8
+
+    const counter = `Прописных ${up}\nСтрочных ${low}`;
     // Прописных 3
     // Строчных 8
     return counter;
-
 }
